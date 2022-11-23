480479f274a3b850dc895b61484cc44a6d3d281e
rivermaners <river.maners@gmail.com>
2022-11-23 17:25:19 -0600

    Updated

--- src/CryptoShop.java
+++ src/CryptoShop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class bitcoin {
+public class CryptoShop {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         MyDate date = new MyDate();
